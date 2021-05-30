a5a20f8886acffe5b086ef9596f8085181ee46a3
Riley Brown <riley@riley.gg>
2021-05-29 22:57:26 -0400

    server price APIs

---
+++ Server/API/price.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch';
+
+export async function getDogeBtcPrice() {
+  const res = await fetch('https://sochain.com/api/v2/get_price/DOGE/BTC');
+  const json = await res.json();
+
+  const binancePrice = json.data.prices.find(
+    (price) => price.exchange === 'binance'
+  );
+
+  return binancePrice;
+}
+
+export async function getDogeUsdPrice() {
+  const res = await fetch('https://sochain.com//api/v2/get_price/DOGE/USD');
+  const json = await res.json();
+
+  const binancePrice = json.data.prices.find(
+    (price) => price.exchange === 'binance'
+  );
+
+  return binancePrice;
+}
