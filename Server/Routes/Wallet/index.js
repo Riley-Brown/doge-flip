ea50ea6de71088ad6b32427fa036bf0cf9148f40
Riley Brown <riley@riley.gg>
2021-05-29 23:03:59 -0400

    wallet route to handle creating, updating, and syncing doge wallet

---
+++ Server/Routes/Wallet/index.js
@@ -0,0 +1,143 @@
+import { Router } from 'express';
+
+import coinInfo from 'coininfo';
+import CoinKey from 'coinkey';
+
+import { mongoClient } from '../../DB';
+
+import { DOGE_NETWORK, getUnspentTx, MAIN_WALLET_PUBLIC_KEY } from '../../API';
+
+import { handleSendDogeCoin } from '../../Components/Transactions';
+
+import { v4 as uuid } from 'uuid';
+
+const router = Router();
+
+router.get('/create', async (req, res) => {
+  const dogeVersions = coinInfo('DOGE-TEST').versions;
+  const key = new CoinKey.createRandom(dogeVersions);
+
+  const userId = uuid();
+
+  const walletsCollection = mongoClient.db('doge-flip').collection('wallets');
+
+  await walletsCollection.insertOne({
+    _id: userId,
+    publicAddress: key.publicAddress,
+    privateWif: key.privateWif,
+    privateHex: key.privateKey.toString('hex'),
+    balance: 0,
+    network: DOGE_NETWORK,
+    userId
+  });
+
+  res.send({ type: 'ok', data: { publicAddress: key.publicAddress, userId } });
+});
+
+router.post('/update', async (req, res) => {
+  const { publicAddress, displayName } = req.body;
+
+  const walletsCollection = mongoClient.db('doge-flip').collection('wallets');
+
+  const wallet = await walletsCollection.findOne({
+    publicAddress
+  });
+
+  if (!wallet) {
+    return res
+      .status(400)
+      .json({ type: 'error', message: 'Wallet does not exist' });
+  }
+
+  if (!displayName || typeof displayName !== 'string') {
+    return res
+      .status(400)
+      .json({ type: 'error', message: 'Invalid parameters' });
+  }
+
+  await wallet.findOneAndUpdate({ publicAddress }, { $set: { displayName } });
+
+  res.send({ type: 'ok' });
+});
+
+router.post('/', async (req, res) => {
+  const walletsCollection = mongoClient.db('doge-flip').collection('wallets');
+
+  const data = await walletsCollection.findOne({
+    publicAddress: req.body.publicAddress
+  });
+
+  res.send({
+    type: 'ok',
+    data: {
+      publicAddress: data.publicAddress,
+      balance: data.balance,
+      userId: data.userId,
+      network: data.network
+    }
+  });
+});
+
+router.post('/sync-wallet', async (req, res) => {
+  const { publicAddress } = req.body;
+
+  const walletsCollection = mongoClient.db('doge-flip').collection('wallets');
+
+  const wallet = await walletsCollection.findOne({ publicAddress });
+
+  if (!wallet) {
+    return res.send({ type: 'error', message: 'This wallet does not exist' });
+  }
+
+  const unspentTx = await getUnspentTx({
+    pubAddress: publicAddress,
+    network: DOGE_NETWORK
+  });
+
+  if (unspentTx.status === 'success' && unspentTx.data.txs.length > 0) {
+    let totalUnspentValue = 0;
+    unspentTx.data.txs.forEach(
+      (element) => (totalUnspentValue += Number(element.value))
+    );
+
+    const sendToMainWallet = await handleSendDogeCoin({
+      dogeCoinsToSend: totalUnspentValue,
+      network: DOGE_NETWORK,
+      receiverAddress: MAIN_WALLET_PUBLIC_KEY,
+      sourceAddress: wallet.publicAddress,
+      privateKey: wallet.privateWif
+    });
+
+    if (sendToMainWallet?.status === 'success') {
+      const updateWallet = await walletsCollection.findOneAndUpdate(
+        {
+          publicAddress
+        },
+        { $inc: { balance: totalUnspentValue } },
+        { returnDocument: 'after' }
+      );
+
+      return res.send({
+        type: 'ok',
+        data: {
+          publicAddress: updateWallet.value.publicAddress,
+          userId: updateWallet.value.userId,
+          balance: updateWallet.value.balance,
+          network: updateWallet.value.network
+        }
+      });
+    }
+  }
+
+  res.send({
+    type: 'ok',
+    data: {
+      publicAddress: wallet.publicAddress,
+      balance: wallet.balance,
+      userId: wallet.userId,
+      network: wallet.network
+    }
+  });
+});
+
+export default router;
