a1073ef26b693aa8ff778c2422883ddf34d2e5a4
Riley Brown <riley@riley.gg>
2021-05-29 22:57:41 -0400

    server root constant exports

---
+++ Server/API/root.js
@@ -0,0 +1,4 @@
+export const APP_MODE = process.env.NODE_ENV === 'production' ? 'live' : 'test';
+export const DOGE_NETWORK = APP_MODE === 'test' ? 'DOGETEST' : 'DOGE';
+export const BTC_NETWORK = APP_MODE === 'test' ? 'BTCTEST' : 'BTC';
+export const MAIN_WALLET_PUBLIC_KEY = process.env.MAIN_WALLET_PUBLIC_KEY;
