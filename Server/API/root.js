4103ec1641e3bb789f35c65e044832f0450c3dd2
Riley Brown <riley@riley.gg>
2021-05-31 18:32:52 -0400

    adjust server app mode conditional

--- Server/API/root.js
+++ Server/API/root.js
@@ -1,4 +1,5 @@
-export const APP_MODE = process.env.NODE_ENV === 'production' ? 'live' : 'test';
+// export const APP_MODE = process.env.NODE_ENV === 'production' ? 'live' : 'test';
+export const APP_MODE = process.env.APP_MODE || 'test';
 export const DOGE_NETWORK = APP_MODE === 'test' ? 'DOGETEST' : 'DOGE';
 export const BTC_NETWORK = APP_MODE === 'test' ? 'BTCTEST' : 'BTC';
 export const MAIN_WALLET_PUBLIC_KEY = process.env.MAIN_WALLET_PUBLIC_KEY;
