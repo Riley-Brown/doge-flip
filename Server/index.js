e8e0f85867d0164088db053047546424d4204373
Riley Brown <riley@riley.gg>
2021-06-03 14:29:06 -0400

    add cookie parser and auth middlewares, add cors options

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 import Wallet from './Routes/Wallet';
 import Transactions from './Routes/Transactions';
 import CoinFlips from './Routes/CoinFlips';
 
-import { mongoClient } from './DB';
+import { setUserCookie } from './Middleware/authMiddleware';
+
+const corsOptions = {
+  credentials: true,
+  origin: [
+    'http://localhost:3000',
+    'https://competent-shannon-ea4b21.netlify.app'
+  ]
+};
 
 const server = express();
 server.use(express.json());
-server.use(cors());
+server.use(cors(corsOptions));
 server.use(morgan('dev'));
+server.use(cookieParser());
 
-server.use('/wallet', Wallet);
+server.use('/wallet', setUserCookie, Wallet);
 server.use('/transactions', Transactions);
 server.use('/coin-flips', CoinFlips);
 
