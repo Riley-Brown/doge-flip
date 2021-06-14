370fd26974a6f6ef58dd9330d7c61b70054a4247
Riley Brown <riley@riley.gg>
2021-06-13 21:12:14 -0400

    export main wallet private key in server API root

--- Server/API/root.js
+++ Server/API/root.js
@@ -3,3 +3,4 @@ export const APP_MODE = process.env.APP_MODE || 'test';
 export const DOGE_NETWORK = APP_MODE === 'test' ? 'DOGETEST' : 'DOGE';
 export const BTC_NETWORK = APP_MODE === 'test' ? 'BTCTEST' : 'BTC';
 export const MAIN_WALLET_PUBLIC_KEY = process.env.MAIN_WALLET_PUBLIC_KEY;
+export const MAIN_WALLET_PRIVATE_KEY = process.env.MAIN_WALLET_PRIVATE_KEY;
