9fd0155edd664863299814b4c35cef6feb8273ff
Riley Brown <riley@riley.gg>
2021-06-27 22:56:24 -0400

    add API call to get cached chat messages, add total chatters UI and socket logic

--- front-end/src/Components/Chat/index.tsx
+++ front-end/src/Components/Chat/index.tsx
@@ -7,16 +7,23 @@ import { ReactComponent as ChevronUp } from 'Assets/chevron-up.svg';
 import { ReactComponent as ChevronDown } from 'Assets/chevron-down.svg';
 
 import './Chat.scss';
+import { getChatMessages } from 'API/chat';
 
 export default function Chat() {
   const [chat, setChat] = useState<any[]>([]);
   const [input, setInput] = useState('');
+
+  const [totalChatters, setTotalChatters] = useState(0);
 
   const [open, setOpen] = useState(false);
 
   const socket = useRef<Socket>();
 
   const account = useTypedSelector((state) => state.account);
+
+  useEffect(() => {
+    getChatMessages().then(({ data }) => setChat(data.messages));
+  }, []);
 
   useEffect(() => {
     const chatContainer = document.querySelector('.chat-container');
@@ -33,10 +40,15 @@ export default function Chat() {
   useEffect(() => {
     if (!account.userId) return;
 
-    socket.current = io(`${API_ROOT}`, { path: '/chat' });
+    socket.current = io(`${API_ROOT}`, { path: '/socket/chat' });
 
     socket.current?.on('chatMessage', (message) => {
       setChat((prev) => [...prev, message]);
+    });
+
+    socket.current.on('totalChatters', (total) => {
+      console.log(total);
+      setTotalChatters(total);
     });
   }, [account.userId]);
 
@@ -58,6 +70,7 @@ export default function Chat() {
         <div className="container">
           <div className="header">
             <h1>Chat</h1>
+            <span style={{ marginLeft: '20px' }}>{totalChatters} online</span>
             <button className="btn close-button" onClick={() => setOpen(false)}>
               &times;
             </button>
