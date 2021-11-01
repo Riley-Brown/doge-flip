c9a2dd232f534be57638ea9db02b7e09b85ec3fc
Riley Brown <riley@riley.gg>
2021-10-31 21:58:20 -0400

    add missing doge network in send transaction api

--- Server/API/send.js
+++ Server/API/send.js
@@ -1,13 +1,17 @@
 import fetch from 'node-fetch';
+import { DOGE_NETWORK } from './root';
 
-export async function sendTransaction({ txHex, network }) {
-  const res = await fetch(`https://sochain.com/api/v2/send_tx/${network}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ tx_hex: txHex })
-  });
+export async function sendTransaction({ txHex }) {
+  const res = await fetch(
+    `https://sochain.com/api/v2/send_tx/${DOGE_NETWORK}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ tx_hex: txHex }),
+    }
+  );
 
   const json = await res.json();
   return json;
