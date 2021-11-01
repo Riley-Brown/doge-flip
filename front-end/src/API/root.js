5efdd923ee12bbeafab561fc4b409008472754ff
Riley Brown <riley@riley.gg>
2021-10-31 21:40:53 -0400

    app mode export

--- front-end/src/API/root.js
+++ front-end/src/API/root.js
@@ -1,3 +1,4 @@
 export const API_ROOT =
   process.env.REACT_APP_API_ROOT || 'http://localhost:9999';
 export const WS_ROOT = process.env.REACT_APP_WS_ROOT || 'ws://localhost:9999';
+export const APP_MODE = process.env.REACT_APP_APP_MODE;
