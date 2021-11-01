49b67b0a8a41dbeb3f5754abcdd188e91fe07d4c
Riley Brown <riley@riley.gg>
2021-10-31 21:42:28 -0400

    adjust doge network root export

--- Server/API/root.js
+++ Server/API/root.js
@@ -1,6 +1,6 @@
 // export const APP_MODE = process.env.NODE_ENV === 'production' ? 'live' : 'test';
 export const APP_MODE = process.env.APP_MODE || 'test';
-export const DOGE_NETWORK = APP_MODE === 'test' ? 'DOGETEST' : 'DOGE';
+export const DOGE_NETWORK = APP_MODE === 'live' ? 'DOGE' : 'DOGETEST';
 export const BTC_NETWORK = APP_MODE === 'test' ? 'BTCTEST' : 'BTC';
 export const MAIN_WALLET_PUBLIC_KEY = process.env.MAIN_WALLET_PUBLIC_KEY;
 export const MAIN_WALLET_PRIVATE_KEY = process.env.MAIN_WALLET_PRIVATE_KEY;
