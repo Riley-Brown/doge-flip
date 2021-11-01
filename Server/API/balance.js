9c16ee237bc4ab5ada0349935853448bd2fa40f4
Riley Brown <riley@riley.gg>
2021-10-31 21:41:49 -0400

    remove unneeded network api params

--- Server/API/balance.js
+++ Server/API/balance.js
@@ -1,27 +1,25 @@
 import fetch from 'node-fetch';
-import { APP_MODE, DOGE_NETWORK } from './root.js';
+import { DOGE_NETWORK } from './root.js';
 
-console.log(APP_MODE);
-
-export async function getBalance({ pubAddress, network }) {
+export async function getBalance({ pubAddress }) {
   const res = await fetch(
-    `https://sochain.com/api/v2/get_address_balance/${network}/${pubAddress}`
+    `https://sochain.com/api/v2/get_address_balance/${DOGE_NETWORK}/${pubAddress}`
   );
   const json = await res.json();
   return json;
 }
 
-export async function getUnspentTx({ pubAddress, network }) {
+export async function getUnspentTx({ pubAddress }) {
   const res = await fetch(
-    `https://sochain.com/api/v2/get_tx_unspent/${network}/${pubAddress}`
+    `https://sochain.com/api/v2/get_tx_unspent/${DOGE_NETWORK}/${pubAddress}`
   );
   const json = await res.json();
   return json;
 }
 
-export async function isTxOutputSpent({ txId, network, outputNo }) {
+export async function isTxOutputSpent({ txId, outputNo }) {
   const res = await fetch(
-    `https://sochain.com/api/v2/is_tx_spent/${network}/${txId}/${outputNo}`
+    `https://sochain.com/api/v2/is_tx_spent/${DOGE_NETWORK}/${txId}/${outputNo}`
   );
   const json = await res.json();
   return json;
