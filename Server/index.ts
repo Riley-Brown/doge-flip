70d2b5311d3145de6dae99bf3829c89aa446f61a
Riley Brown <riley@riley.gg>
2021-12-05 17:55:30 -0500

    update index server file to .ts, change to absolute imports

--- Server/index.ts
+++ Server/index.ts
@@ -3,22 +3,22 @@ import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 
-import Wallet from './Routes/Wallet';
-import Transactions from './Routes/Transactions';
-import CoinFlips from './Routes/CoinFlips';
-import Chat, { initSocketIo } from './Routes/Chat';
+import Wallet from 'Routes/Wallet';
+import Transactions from 'Routes/Transactions';
+import CoinFlips from 'Routes/CoinFlips';
+import Chat, { initSocketIo } from 'Routes/Chat';
 
-import { requireUserAuth } from './Middleware/authMiddleware';
+import { requireUserAuth } from 'Middleware/authMiddleware';
 
 export const corsOrigins = [
   'http://localhost:3000',
   'https://competent-shannon-ea4b21.netlify.app',
-  'https://doge-flip.riley.gg'
+  'https://doge-flip.riley.gg',
 ];
 
 const corsOptions = {
   credentials: true,
-  origin: corsOrigins
+  origin: corsOrigins,
 };
 
 const PORT = process.env.PORT || 9999;
