152176472ba5884f76232987da2c6f1fb524b277
Riley Brown <riley@riley.gg>
2021-12-04 20:23:37 -0500

    adjust requireUserAuth middleware to return err on invalid token, add func to set user token from cookie

--- Server/Middleware/authMiddleware.js
+++ Server/Middleware/authMiddleware.js
@@ -8,13 +8,26 @@ export const requireUserAuth = async (req, res, next) => {
 
   try {
     if (!token) {
-      res.locals.userTokenObject = { userId: null, publicAddress: null };
-    } else {
-      res.locals.userTokenObject = await handleVerifyToken(token);
+      return handleSendAuthError(res);
     }
+
+    res.locals.userTokenObject = await handleVerifyToken(token);
+    next();
   } catch (err) {
     console.log(err);
-    res.locals.userTokenObject = { userId: null, publicAddress: null };
+    return handleSendAuthError(res);
+  }
+};
+
+export const setUserTokenFromCookie = async (req, res, next) => {
+  const token = req.cookies?.userToken;
+  const defaultUserToken = { userId: null, publicAddress: null };
+
+  res.locals.userTokenObject = defaultUserToken;
+
+  try {
+    if (!token) return;
+    res.locals.userTokenObject = await handleVerifyToken(token);
   } finally {
     next();
   }
